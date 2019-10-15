0a2ab7c92cd4a7fb43e2f8260a6c7036d94442a5
marciovz <marciovz@yahoo.com.br>
2019-10-15 19:05:45 -0300

    desafio1 - implementado a rota de edição do projeto

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require('express');
 
 const server = express();
+
 server.use(express.json());
 
 
 //Verifica se ID não é nullo ou diferente de string
 function isValidId(req, res, next) {
-  const { id } = req.body;
+  const { id } = req.params.id ? req.params : req.body;
   if (!id || typeof id !== "string") 
     return res.status(400).json({ error: " ID does not correct"});
   return next();
 }
+
 
 //Verifica se Title não é nullo ou diferente de string
 function isValidTitle(req, res, next) {
@@ -36,16 +38,17 @@ let projects = [
 
 ];
 
-//Lista todos os projetos
+
+//Rota para listar todos os projetos
 server.get('/projects', (req, res) => {
   return res.json(projects);
 });
 
-//Cria um novo projeto
+
+//Rota para adicionar um novo projeto
 server.post('/projects', isValidId, isValidTitle, (req, res) => {
   const { id, title } = req.body;
   
-  // Verifica se já existe o id de projeto na lista
   const project = projects.find(project => project.id === id);
   if (project) return res.status(400).json({ error: "Id do projeto já existe"});
 
@@ -60,6 +63,23 @@ server.post('/projects', isValidId, isValidTitle, (req, res) => {
   return res.json(projects);
 });
 
+
+//Rota para editar um projeto
+server.put('/projects/:id', isValidId, isValidTitle, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(project => project.id === id);
+
+  if (!project) return res.status(400).json({ error: "Projeto não encontrado!"});
+
+  project.title = title;
+  
+  return res.json(projects);
+});
+
+
+//Inicia o servidor
 server.listen(3000, (err)=>{
   if (err) return err;
   console.log("Rodando na porta 3000");
