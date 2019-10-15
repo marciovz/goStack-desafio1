d2246ce273f59a2e570363cc4973044a634c3f39
marciovz <marciovz@yahoo.com.br>
2019-10-15 17:37:26 -0300

    desafio1 - implementado a rota de criação de projects

--- index.js
+++ index.js
@@ -3,6 +3,25 @@ const express = require('express');
 const server = express();
 server.use(express.json());
 
+
+//Verifica se ID não é nullo ou diferente de string
+function isValidId(req, res, next) {
+  const { id } = req.body;
+  if (!id || typeof id !== "string") 
+    return res.status(400).json({ error: " ID does not correct"});
+  return next();
+}
+
+//Verifica se Title não é nullo ou diferente de string
+function isValidTitle(req, res, next) {
+  const { title } = req.body;
+  if (!title || typeof title !== "string") 
+    return res.status(400).json({ error: " Title does not correct"});
+  return next();
+}
+
+
+// Lista de projetos
 let projects = [
   {
     id: "1",
@@ -17,7 +36,27 @@ let projects = [
 
 ];
 
-server.get('/', (req, res) => {
+//Lista todos os projetos
+server.get('/projects', (req, res) => {
+  return res.json(projects);
+});
+
+//Cria um novo projeto
+server.post('/projects', isValidId, isValidTitle, (req, res) => {
+  const { id, title } = req.body;
+  
+  // Verifica se já existe o id de projeto na lista
+  const project = projects.find(project => project.id === id);
+  if (project) return res.status(400).json({ error: "Id do projeto já existe"});
+
+  const newProject = {
+    id,
+    title,
+    tasks: [],
+  };
+
+  projects.push(newProject);
+ 
   return res.json(projects);
 });
 
